843f2bec4473b19f12cf0a07ba83ae4a354e6e3b
Brent Rogers <contact@brentjrogers.com>
2016-04-30 21:32:42 -0400

    Update font.js

--- js/font.js
+++ js/font.js
@@ -1,5 +1,70 @@
-WebFont.load({
-          google: {
-          families: ['AvenirNext-Regular', 'sans-serif']
-           }
-          });
+<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
+
+  WebFont.load({
+    google: {
+      families: ['Proxima Nova', 'sans-serif']
+    }
+  });
+
+  WebFont.load({
+    google: {
+      families: ['AvenirNext-Regular', 'sans-serif']
+    }
+  });
+
+  WebFont.load({
+    google: {
+      families: ['AvenirNext-Italic', 'sans-serif']
+    }
+  });
+
+  WebFont.load({
+    google: {
+      families: ['Avenir-Light', 'sans-serif']
+    }
+  });
+  
+  WebFont.load({
+    google: {
+      families: ['Avenir-Book', 'sans-serif']
+    }
+  });
+  
+  WebFont.load({
+    google: {
+      families: ['AvenirNextCondensed-UltraLight', 'sans-serif']
+    }
+  });
+  
+  WebFont.load({
+    google: {
+      families: ['Helvetica-Light', 'Helvetica']
+    }
+  });
+  
+  WebFont.load({
+    google: {
+      families: ['HelveticaNeue-UltraLight', 'HelveticaNeue']
+    }
+  });
+  
+  WebFont.load({
+    google: {
+      families: ['NoteWorthy-Bold', 'NoteWorthy']
+    }
+  });
+  
+  WebFont.load({
+    google: {
+      families: ['PingFangTC-Thin', 'PingFangTC']
+    }
+  });
+  
+  WebFont.load({
+    google: {
+      families: ['PingFangHK-Thin', 'PingFangHK']
+    }
+  });
+  
+  
+  
